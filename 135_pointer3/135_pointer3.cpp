8d8ec3f103ff741b24b3156a5942e0c9c3663139
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-05-08 11:34:43 +0700

    anu

--- 135_pointer3/135_pointer3.cpp
+++ 135_pointer3/135_pointer3.cpp
@@ -10,9 +10,26 @@ private:
 public:
     void setNama(string pNama);
     friend class siswa;
-
-
 };
+
+class siswa {
+private:
+    int id;
+public:
+    void setid(int Pid);
+    void displayAll(orang& a);
+};
+
+void siswa::displayAll(orang& a) {
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama) {
+    nama = pNama;
+}
+void siswa::setid(int pid) {
+    id = pid;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
