be0c815e75905e31cd35c1b4ec0884fe74d24b8e
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-05-08 10:49:12 +0700

    menambahkan class mahasiswa

--- 135_Pointer/135_Pointer.cpp
+++ 135_Pointer/135_Pointer.cpp
@@ -1,8 +1,13 @@
-// 135_Pointer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
+class mahasiswa{
+    public:
+    int nim;
+    void showNim() {
+        cout << "No induk = " << nim << endl;
+    }
+};
 int main()
 {
     std::cout << "Hello World!\n";
